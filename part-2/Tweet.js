46b95288462c0a2fa8018a844f885e218c9a4164
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-15 15:24:09 -0500

    started pt2

---
+++ part-2/Tweet.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function Tweet({ username, name, date, message }) {
+  return
+  <div>
+    <ul>
+      <li> {username} </li>
+      <li> {name} </li>
+      <li> {date} </li>
+      <li> {message} </li>
+    </ul>
+  </div>
+
+}
